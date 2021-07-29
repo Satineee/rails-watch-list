28725ad0c5cb0a52695bddaf4a4eebf80975c8c0
satine fouque <satine.fouque@gmail.com>
2021-07-29 16:52:26 +0400

    ajout controllers bookmark et list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get  '/lists',     to: 'lists#index'
+  get  '/lists/new', to: 'lists#new',  as: 'new_list'
+  get  '/lists/:id', to: 'lists#show', as: 'list'
+  post '/lists',     to: 'lists#create'
+
+  get    '/lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: 'new_bookmark'
+  post   '/lists/:list_id/bookmarks',      to: 'bookmarks#create', as: 'list_bookmarks'
+  delete '/bookmarks/:list_id',            to: 'bookmarks#destroy'
 end
